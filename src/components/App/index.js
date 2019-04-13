908c9e1c87a7a4a8b2229c3b8592506995eeb185
Harris Schneiderman <harris.schneiderman@deque.com>
2019-04-13 07:57:35 -0700

    feat: adds logo

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,13 +3,17 @@ import PropTypes from 'prop-types';
 import { TopBar, Main, SkipLink, MenuItem, Layout } from 'cauldron-react';
 import Stats from '../Stats';
 import Recipes from '../Recipes';
+import logo from '../../img/icons/logo.svg';
 import './index.css';
 
 const App = ({ recipes, stats, updateRecipe }) => (
   <div className="App">
     <SkipLink target={'#main-content'} />
     <TopBar role="banner">
-      <MenuItem>awesome recipes</MenuItem>
+      <MenuItem>
+        <img alt="" role="presentation" src={logo} />
+        <span>awesome recipes</span>
+      </MenuItem>
     </TopBar>
     <Layout>
       <Main id="main-content" aria-labelledby="main-heading" tabIndex={-1}>
