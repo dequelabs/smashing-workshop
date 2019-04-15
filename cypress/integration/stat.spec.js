47d8c33482a0626dca851c4e206c2a170082c7ec
Paul Grenier <pgrenier@gmail.com>
2019-04-15 12:43:27 -0400

    chore(tests): adds test for semantic meaning (#3)
    - adds tests for stat counts from data file
    - adds command findByAccessibleName
    - example uses Cypress-provided lodash (good comparison to user implementation)

--- cypress/integration/stat.spec.js
+++ cypress/integration/stat.spec.js
@@ -1,4 +1,6 @@
 const { selectors } = JSON.parse(Cypress.env('CONFIG'));
+const { _ } = Cypress;
+import data from '../../src/data';
 
 describe('stat component', function() {
   it('has a presentational image', function() {
@@ -9,5 +11,21 @@ describe('stat component', function() {
           .invoke('attr', 'alt')
           .should('equal', '');
       });
+  });
+  it('egg used: displays', function() {
+    // handle parsing ingredients in unit tests
+    cy.findByAccessibleName('eggs used').should('have.length', 1);
+  });
+  it('cook count: displays the correct data', function() {
+    const total = _.sumBy(data, d => d.cookCount);
+    cy.findByAccessibleName(`${total} recipes made`).should('have.length', 1);
+  });
+  it('grease fires: displays the correct data', function() {
+    const total = _.sumBy(data, d => d.greaseFireCount);
+    cy.findByAccessibleName(`${total} grease fires`).should('have.length', 1);
+  });
+  it('yumminess: displays the correct data', function() {
+    const avg = _.meanBy(data, d => d.yumminess);
+    cy.findByAccessibleName(`yumminess ${avg}`).should('have.length', 1);
   });
 });
