38f7957de2a654ef047a8c00dc0499fa2b7bfc1d
Harris Schneiderman <schne324@gmail.com>
2020-09-03 08:38:37 -0700

    basic modal implementation

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,81 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { createPortal } from 'react-dom';
 import { TopBar, Main, SkipLink, MenuItem, Layout } from 'cauldron-react';
 import logo from '../../img/icons/logo.svg';
 import './index.css';
 
+const Modal = ({ onClose }) => {
+  const heading = useRef(null);
+  const close = useRef(null);
+  const cancel = useRef(null);
+  const onKeyDown = e => {
+    switch (e.key) {
+      case 'Escape':
+        onClose();
+        break;
+      case 'Tab':
+        if (e.shiftKey && [heading.current, close.current].includes(e.target)) {
+          e.preventDefault();
+          cancel.current.focus();
+        } else if (!e.shiftKey && e.target === cancel.current) {
+          e.preventDefault();
+          close.current.focus();
+        }
+        break;
+    }
+  };
+
+  useEffect(() => {
+    heading.current.focus();
+    document.getElementById('app').setAttribute('aria-hidden', 'true');
+  }, []);
+
+  const modalContent = (
+    <div role="dialog" aria-labelledby="recipe-name" onKeyDown={onKeyDown}>
+      <button
+        ref={close}
+        onClick={onClose}
+        aria-label="Close"
+        className="close"
+      >
+        x
+      </button>
+      <h2 ref={heading} id="recipe-name" tabIndex={-1}>
+        Recipe name
+      </h2>
+      <ul>
+        <li>foo</li>
+        <li>bar</li>
+        <li>baz</li>
+      </ul>
+      <button>save</button>
+      <button ref={cancel} onClick={onClose}>
+        cancel
+      </button>
+    </div>
+  );
+  return createPortal(modalContent, document.getElementById('modal'));
+};
+
 const App = () => {
+  const [open, setOpen] = useState(false);
+  const didMount = useRef(false);
+  const trigger = useRef(null);
+  const onClick = () => setOpen(true);
+  const onClose = () => setOpen(false);
+
+  useEffect(() => {
+    if (!didMount.current) {
+      didMount.current = true;
+      return;
+    }
+
+    if (!open) {
+      document.getElementById('app').removeAttribute('aria-hidden');
+      trigger.current.focus();
+    }
+  }, [open]);
+
   return (
     <div className="App">
       <SkipLink target={'#main-content'} />
@@ -21,8 +93,11 @@ const App = () => {
             </div>
           </div>
           <div className="App__body">
-            <button>Trigger Modal!</button>
+            <button ref={trigger} onClick={onClick}>
+              Trigger Modal!
+            </button>
           </div>
+          {open && <Modal onClose={onClose} />}
         </Main>
       </Layout>
     </div>
